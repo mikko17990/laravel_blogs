4361bef552321cc15ade5d888e2af7aa95fc21c4
mikko17990 <50828811+mikko17990@users.noreply.github.com>
2021-03-24 23:50:09 +0700

    create table

---
+++ app/Http/Controllers/MommentController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\momment;
+use Illuminate\Http\Request;
+
+class MommentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\momment  $momment
+     * @return \Illuminate\Http\Response
+     */
+    public function show(momment $momment)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\momment  $momment
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(momment $momment)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\momment  $momment
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, momment $momment)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\momment  $momment
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(momment $momment)
+    {
+        //
+    }
+}
